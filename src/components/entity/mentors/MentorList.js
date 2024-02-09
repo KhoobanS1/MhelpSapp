e7660c32d139b8d29d3d151585ad19daa1db799d
KhoobanS1 <121337927+KhoobanS1@users.noreply.github.com>
2024-02-09 09:40:54 +0000

    populated data and shows in finding mentor page

---
+++ src/components/entity/mentors/MentorList.js
@@ -0,0 +1,23 @@
+import { ScrollView, StyleSheet} from 'react-native';
+import MentorItem from './MentorItem';
+
+
+const MentorList = ({mentors,onSelect}) => {
+// Initialisations ---------------------
+// State -------------------------------
+// Handlers ----------------------------
+// View --------------------------------
+return (
+
+
+<ScrollView style={styles.container}>
+{mentors.map((mentor) => {
+  return <MentorItem key={mentor.MentorID}  mentor={mentor} onSelect={onSelect}/>;
+})}
+</ScrollView>
+
+);
+};
+const styles = StyleSheet.create({});
+
+export default MentorList;
---
+++ src/components/screens/MentorFindScreen.js
@@ -0,0 +1,27 @@
+import { StyleSheet} from 'react-native';
+import Screen from '../layout/Screen';
+import initialMentors from '../../data/mentors.js';
+import MentorList from '../entity/mentors/MentorList.js';
+
+const MentorFindScreen =()=> {
+
+// Initialisations -------------------
+const mentors = initialMentors;
+// State -----------------------------
+// Handlers --------------------------
+const handleSelect = (mentors)=> alert(`Item ${mentors.MentorsName} selected`);
+//View -------------------------------
+  return (
+    <Screen>
+      <MentorList mentors={mentors} onSelect={handleSelect}/>
+    </Screen>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{},
+ 
+
+});
+
+export default MentorFindScreen;
